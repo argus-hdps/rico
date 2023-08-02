73ca16ba5b1e293d1290e9681d0aa651afaec728
Hank Corbett <htc@unc.edu>
2023-08-02 04:24:51 +0100

    typo in avro schema

--- src/argus_rico/efte/stream.py
+++ src/argus_rico/efte/stream.py
@@ -1,4 +1,4 @@
-__all__ = ["RawAlertStreamer"]
+__all__ = ["EFTEAlertReceiver", "EFTEAlertStreamer"]
 
 import base64
 import io
@@ -18,80 +18,6 @@ from ..consumer import Consumer
 from ..producer import Producer
 
 PATH = os.path.realpath(os.path.dirname(__file__))
-
-
-class RawAlertStreamer(Producer):
-    def __init__(self) -> None:
-        """
-        Initialize the RawAlertStreamer instance.
-
-        It inherits from the Producer class and configures the Kafka connection parameters
-        using values from the config dictionary.
-
-        This class is used for streaming raw candidate detections from the
-        observatory to the Rico Kafka cluster.
-
-        The host, port, and topic are retrieved from the config dictionary
-        using the keys 'KAFKA_ADDR', 'KAFKA_PORT', and 'HBEAT_TOPIC' respectively.
-        """
-        super().__init__(
-            host=config.KAFKA_ADDR,
-            port=config.KAFKA_PORT,
-        )
-        self.topic_base = config.RAW_TOPIC_BASE
-
-    def _parse_raw_catalog(self, catalog_path: str) -> Tuple[bytes, Dict[str, Any]]:
-        """
-        Parses a raw catalog file and returns the serialized Avro data.
-
-        Args:
-            catalog_path (str): The path to the catalog file.
-
-        Returns:
-            bytes: The serialized Avro data.
-            dict: Catalog metadata.
-        """
-        tab = tbl.Table.read(catalog_path)
-
-        mjd = tab.meta["MJD"]
-        camera_id = tab.meta["CCDDETID"]
-
-        records = []
-        for r in tab:
-            stamp = blosc.compress(r["stamp"].tobytes())
-            record = dict(r)
-            record["stamp_bytes"] = stamp
-            record["epoch"] = mjd
-            record["camera"] = camera_id
-
-            records.append(record)
-
-        with open(f"{PATH}/schemas/raw_candidate.avsc", "rb") as f:
-            schema = orjson.loads(f.read())
-
-        parsed_schema = fa.parse_schema(schema)
-
-        fo = io.BytesIO()
-        fa.writer(fo, parsed_schema, records)
-        fo.seek(0)
-
-        return fo.read(), tab.meta
-
-    def push_from_catalog(self, catalog_path: str) -> None:
-        """
-        Pushes data from a catalog file to a camera-specific topic.
-
-        Args:
-            catalog_path (str): The path to the catalog file.
-
-        Returns:
-            None
-
-        """
-        avro_data, metadata = self._parse_raw_catalog(catalog_path)
-        topic = self.topic_base + f'.{metadata["CCDDETID"]}'
-
-        self.send_binary(avro_data, topic=topic)
 
 
 class EFTEAlertStreamer(Producer):
@@ -149,7 +75,7 @@ class EFTEAlertStreamer(Producer):
                 "objectId": str(uuid4()),
             }
 
-            stamp = blosc.compress(r["stamp"].tobytes())
+            stamp = blosc.compress(r["stamp"].data.tobytes())
 
             candidate = dict(r)
             candidate["stamp_bytes"] = stamp
